73913f4a0ce3ae52d1e3e3b9ae7808d3a85d5ee8
lordponchik <79088186+lordponchik@users.noreply.github.com>
2021-11-12 23:37:48 +0000

    Deploying to gh-pages from @ lordponchik/team-tear@7b6e38ad97475d019b09b19ffac3b47cccf08ab5 🚀

--- src.2f0c6426.js
+++ src.2f0c6426.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-tear/src.d86e5d57.js.map
+//# sourceMappingURL=/team-tear/src.2f0c6426.js.map
