eafcaa75eaef616352521a63ddf8781872f05f01
Denis Strashnyuk <strashnyuk.denis@gmail.com>
2021-11-18 02:45:54 +0200

    bugfix1.0

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,11 @@
-(() => {
-  const swiper = new Swiper('.mySwiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    keyboard: {
-      enabled: true,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  })();
-})();
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
